6f38a2fb6060f71c493ebbeb2d5c70ce94d2d642
Kubi Dogan <kubi_g@hotmail.co.uk>
2022-12-06 19:55:50 +0000

    styling views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+
+
   resources :members
   resources :parties
   devise_scope :user do
     # Redirests signing out users back to sign-in
+
     get "users", to: "devise/sessions#new"
-    root to: "devise/sessions#create"
+
+
   end
 devise_for :users
 end
